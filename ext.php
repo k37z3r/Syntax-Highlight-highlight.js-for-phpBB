eeb3d1deb16e00791c7ce18cd1a4661eae9434eb
Sven Minio <105192630+k37z3r@users.noreply.github.com>
2024-08-27 17:20:53 +0200

    Add files via upload

---
+++ ext.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *
+ * Syntax Highlight (highlight.js). An extension for the phpBB Forum Software package.
+ *
+ * @author Sven Minio
+ * @copyright (c) 2024, Sven Minio, https://source-collab.com
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ * For full copyright and license information, please see
+ * the license.txt file.
+ */
+
+namespace k37z3r\syntaxhightlight;
+
+/**
+ * Syntax Highlight (highlight.js) Extension base
+ *
+ * It is recommended to remove this file from
+ * an extension if it is not going to be used.
+ */
+class ext extends \phpbb\extension\base
+{
+}
